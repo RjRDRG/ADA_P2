e4fccc66ea909c43a70680371ad4c0e8dd0bd8e9
Rodrigo Ribeiro <jr.rodrigo.pt@gmail.com>
2021-05-11 23:23:38 +0100

    sa

--- src/Legionellosis.java
+++ src/Legionellosis.java
@@ -1,21 +1,60 @@
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Legionellosis {
 
     List<Integer>[] adjacencies;
     Map<Integer,Integer> sick;
+    final int nsick;
 
-    public Legionellosis(List<Integer>[] adjacencies, Map<Integer,Integer> sick) {
+    public Legionellosis(List<Integer>[] adjacencies, Map<Integer,Integer> sick, int nsick) {
         this.adjacencies = adjacencies;
         this.sick = sick;
+        this.nsick = nsick;
     }
 
     Set<Integer> solve() {
-        for(int i = 0; i< sick.size(); i++){
+        int[] sickCounter = new int[adjacencies.length];
+        Set<Integer> perilousLocations = new HashSet<>();
 
+        for(Map.Entry<Integer,Integer> entry: sick.entrySet()){
+            Queue<RelativeLocation> frontier = new LinkedList<>();
+            Set<Integer> explored = new HashSet<>();
+
+            int home = entry.getKey();
+            int maxDistance = entry.getValue();
+
+            frontier.add(new RelativeLocation(home,0));
+            explored.add(home);
+
+            while (!frontier.isEmpty()) {
+                RelativeLocation currentLocation = frontier.poll();
+
+                sickCounter[currentLocation.index] += 1;
+
+                if (sickCounter[currentLocation.index]==nsick)
+                    perilousLocations.add(currentLocation.index);
+
+                for (Integer adj : adjacencies[currentLocation.index]) {
+                    int newDistance = currentLocation.distance+1;
+                    if(newDistance <= maxDistance && !explored.contains(adj)) {
+                        frontier.add(new RelativeLocation(adj, newDistance));
+                        explored.add(adj);
+                    }
+                }
+            }
         }
 
+
+        return perilousLocations;
+    }
+
+    public static class RelativeLocation {
+        public int index;
+        public int distance;
+
+        public RelativeLocation(int index, int distance) {
+            this.index = index;
+            this.distance = distance;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -18,8 +18,8 @@ public class Main {
 
         for(int i=0; i<nconnections; i++) {
             line = in.readLine().split(" ");
-            int location0 = Integer.parseInt(line[0]);
-            int location1 = Integer.parseInt(line[1]);
+            int location0 = Integer.parseInt(line[0])-1;
+            int location1 = Integer.parseInt(line[1])-1;
 
             if(adjacencies[location0] == null)
                 adjacencies[location0] = new ArrayList<>();
@@ -31,27 +31,102 @@ public class Main {
             adjacencies[location1].add(location0);
         }
 
-        int nsick = Integer.parseInt(in.readLine());
+        final int nsick = Integer.parseInt(in.readLine());
 
         Map<Integer,Integer> sick = new HashMap<>();
 
         for (int i=0; i<nsick; i++) {
             line = in.readLine().split(" ");
-            int home = Integer.parseInt(line[0]);
+            int home = Integer.parseInt(line[0])-1;
             int distance = Integer.parseInt(line[1]);
             sick.put(home,distance);
         }
 
-        Set<Integer> perilousLocations = new Legionellosis(adjacencies,sick).solve();
+        Set<Integer> perilousLocations = new Legionellosis(adjacencies,sick,nsick).solve();
 
-        StringJoiner joiner = new StringJoiner(" ");
-        joiner.add("01").add("02").add("03");
-
-        for (Integer location: perilousLocations) {
-            joiner.add(location.toString());
+        if(perilousLocations.isEmpty())
+            System.out.println(0);
+        else {
+            StringJoiner joiner = new StringJoiner(" ");
+            for (Integer location : perilousLocations) {
+                location++;
+                joiner.add(location.toString());
+            }
+            System.out.println(joiner);
         }
-
-        System.out.println(joiner);
-
     }
 }
+
+/*
+
+16 18
+6 8
+7 3
+16 15
+12 14
+13 12
+2 1
+13 15
+11 12
+6 2
+10 11
+4 7
+5 6
+1 5
+3 4
+3 2
+9 10
+9 8
+8 7
+3
+3 3
+6 2
+11 3
+
+5 5
+1 2
+2 3
+3 4
+4 5
+5 1
+2
+1 1
+4 2
+
+5 6
+1 5
+5 4
+4 3
+3 2
+2 1
+1 3
+3
+1 2
+2 3
+3 1
+
+16 18
+15 16
+2 6
+5 1
+3 7
+12 13
+3 2
+4 3
+10 9
+12 11
+8 6
+7 8
+13 15
+9 8
+14 12
+4 7
+6 5
+2 1
+10 11
+3
+6 2
+11 1
+3 3
+
+ */
